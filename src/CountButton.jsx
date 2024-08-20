8303559acdfbb20d7e419bfebcecdb67acf9b978
Jasmin Raith <jasminraith@icloud.com>
2024-08-20 13:30:08 +0200

    create one reusable button component

---
+++ src/CountButton.jsx
@@ -0,0 +1,18 @@
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+
+export default function CountButton({ type, setCount }) {
+  return (
+    <button
+      onClick={() => {
+        setCount((prev) => prev + 1);
+      }}
+      className="count-btn"
+    >
+      {type === "minus" ? (
+        <MinusIcon className="count-btn-icon" />
+      ) : (
+        <PlusIcon className="count-btn-icon" />
+      )}
+    </button>
+  );
+}
